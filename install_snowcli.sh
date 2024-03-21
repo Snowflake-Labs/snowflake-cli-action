d032ad67dcc853401d955119a77ce364f32a243d
Jorge Vasquez Rojas <jorge.vasquezrojas@snowflake.com>
2024-03-21 12:09:39 -0600

    Install always with pipx

--- install_snowcli.sh
+++ install_snowcli.sh
@@ -2,23 +2,15 @@
 
 set -euo pipefail
 
-echo "CLI VERSION: $CLI_VERSION"
-echo "INSTALL_USING_PIPX: $INSTALL_USING_PIPX"
-pipx list
-
 if command -v snow &> /dev/null
 then
     echo "Snowcli already installed"
     exit 0
 fi
 
-if [ "$INSTALL_USING_PIPX" == true ]; then
-    installation_comand="pipx"
-fi
-
 
 if [ "$CLI_VERSION" == "" ]; then
-    $installation_comand install snowflake-cli-labs 
+    pipx install snowflake-cli-labs 
 else 
-    $installation_comand install snowflake-cli-labs==$CLI_VERSION
+    pipx install snowflake-cli-labs==$CLI_VERSION
 fi
