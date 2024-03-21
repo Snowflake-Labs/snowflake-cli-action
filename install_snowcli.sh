735184dfbc02a87cc70ccd76ca70500d35baf018
Jorge Vasquez Rojas <jorge.vasquezrojas@snowflake.com>
2024-03-21 13:28:33 -0600

    add cache action

--- install_snowcli.sh
+++ install_snowcli.sh
@@ -1,16 +1,2 @@
 ﻿#!/bin/sh
 
-set -euo pipefail
-
-if command -v snow &> /dev/null
-then
-    echo "Snowcli already installed"
-    exit 0
-fi
-
-
-if [ "$CLI_VERSION" == "" ]; then
-    pipx install snowflake-cli-labs 
-else 
-    pipx install snowflake-cli-labs==$CLI_VERSION
-fi
