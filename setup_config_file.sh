e7586e750412cf70f8e2309bf7a898f3bf798d67
Jorge Vasquez Rojas <jorge.vasquezrojas@snowflake.com>
2024-03-22 14:59:32 -0600

    addd echo

--- setup_config_file.sh
+++ setup_config_file.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 mkdir -p ./temp/
 cat $CONFIG_FILE_PATH >> ./temp/config.toml
+echo $(cat ./temp/config.toml)
 
 if [[ ! $RUNNER_OS = "Windows" ]]; then 
     chmod 0600 ./temp/config.toml
