0bbf8e708940438ab128142c40243c5422203e64
Jorge Vasquez Rojas <jorge.vasquezrojas@snowflake.com>
2024-03-22 15:09:32 -0600

    Add cp instead of cat

--- setup_config_file.sh
+++ setup_config_file.sh
@@ -3,8 +3,7 @@
 set -euo pipefail
 
 mkdir -p ./temp/
-cat $CONFIG_FILE_PATH >> ./temp/config.toml
-echo $(cat ./temp/config.toml)
+cp $CONFIG_FILE_PATH ./temp/config.toml
 
 if [[ ! $RUNNER_OS = "Windows" ]]; then 
     chmod 0600 ./temp/config.toml
