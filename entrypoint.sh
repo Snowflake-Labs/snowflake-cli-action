ecbac5c49273e8e5dc06a071553ac332cef049fb
Jorge Morales <jorge.moralescarmiol@snowflake.com>
2024-03-22 14:07:33 -0600

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ cat "$1" >> config.toml
 
 chmod 0600 config.toml
 
-echo $(snow --config-file config.toml connection test) >> result.log
+echo $(snow --config-file config.toml connection test)
 
-echo $(snow --config-file config.toml object stage copy $3 $2 --overwrite) >> result.log
+echo $(snow --config-file config.toml object stage copy $3 $2 --overwrite)
 
 echo "Hello World $0 $1 $2 $3 $4 $5"
