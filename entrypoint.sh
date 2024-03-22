500a16ef6a98db4062e26036e0b4077d85797221
Jorge Morales <jorge.moralescarmiol@snowflake.com>
2024-03-22 13:58:22 -0600

    Testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ echo $(snow --config-file $1 connection test) >> result.log
 
 echo $(snow --config-file $1 object stage copy $3 $2 --overwrite) >> result.log
 
-echo "Hello World $0 $1 $2 $3 $4 $5 USER: $USER"
+echo "Hello World $0 $1 $2 $3 $4 $5"
 
 
