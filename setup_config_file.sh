51de0614df6a37316e46d6fd4a46016f9190f439
Jorge Vasquez Rojas <jorge.vasquezrojas@snowflake.com>
2024-03-22 14:46:49 -0600

    test connection

--- setup_config_file.sh
+++ setup_config_file.sh
@@ -9,7 +9,5 @@ if [[ ! $RUNNER_OS = "Windows" ]]; then
     chmod 0600 ./temp/config.toml
 fi
 
-if [ "$SAVE_CONFIG_FILE_SNOWFLAKE_DIR" = true ]; then
-  mkdir -p ~/.snowflake/
-  mv "$CONFIG_FILE_PATH" ~/.snowflake/
-fi
+mkdir -p ~/.snowflake/
+mv ./temp/config.toml ~/.snowflake/
