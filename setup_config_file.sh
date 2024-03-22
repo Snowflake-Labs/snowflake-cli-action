32957b5ccebf5edf2e1c5ebad28b327f62b448a1
Jorge Vasquez Rojas <jorge.vasquezrojas@snowflake.com>
2024-03-22 14:42:57 -0600

    Add setup command

--- setup_config_file.sh
+++ setup_config_file.sh
@@ -2,11 +2,14 @@
 
 set -euo pipefail
 
+mkdir -p ./temp/
+cp $CONFIG_FILE_PATH ./temp/config.toml
+
 if [[ ! $RUNNER_OS = "Windows" ]]; then 
-    chmod 0600 config.toml
+    chmod 0600 ./temp/config.toml
 fi
 
 if [ "$SAVE_CONFIG_FILE_SNOWFLAKE_DIR" = true ]; then
   mkdir -p ~/.snowflake/
-  mv "$$CONFIG_FILE_PATH" ~/.snowflake/
+  mv "$CONFIG_FILE_PATH" ~/.snowflake/
 fi
