c075b1f40a32e9b62796707e91d5109d92b9f057
Jorge Vasquez Rojas <jorge.vasquezrojas@snowflake.com>
2024-03-22 10:49:23 -0600

    Add caching

--- install_snowcli.sh
+++ install_snowcli.sh
@@ -1,2 +1,17 @@
 ﻿#!/bin/sh
 
+set -euo pipefail
+
+if command -v snow &> /dev/null
+then
+    echo "Snowcli already installed"
+    exit 0
+fi
+
+
+if [ "$CLI_VERSION" == "" ]; then
+    pipx install snowflake-cli-labs --python $PYTHON_VERSION
+fi
+else 
+    pipx install snowflake-cli-labs==$CLI_VERSION --python $PYTHON_VERSION
+fi
