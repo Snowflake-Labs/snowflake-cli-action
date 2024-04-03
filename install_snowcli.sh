01b6a2a33d8c2034c85f9d8ef609ecebdb78fc89
Jorge Vasquez Rojas <jorge.vasquezrojas@snowflake.com>
2024-04-03 09:01:22 -0600

    Update install_snowcli script

--- install_snowcli.sh
+++ install_snowcli.sh
@@ -16,5 +16,7 @@ echo $PIPX_BIN_DIR
 echo $(ls $PIPX_BIN_DIR)
 mkdir $PIPX_BIN_DIR/snow_ensure_pipx_path
 cp $PIPX_BIN_DIR/snow $PIPX_BIN_DIR/snow_ensure_pipx_path
-PATH=$PIPX_BIN_DIR/snow_ensure_pipx_path:$PATH
-echo $(ls $PIPX_BIN_DIR)
+echo "$PIPX_BIN_DIR/snow_ensure_pipx_path" >> $GITHUB_PATH
+
+echo $(ls $PIPX_BIN_DIR/snow_ensure_pipx_path)
+
