efae416b45548e7559d75885165019564ead3ca9
Jorge Vasquez Rojas <jorge.vasquezrojas@snowflake.com>
2024-04-03 09:48:35 -0600

    Add extension to copy command

--- install_snowcli.sh
+++ install_snowcli.sh
@@ -8,14 +8,13 @@ else
     pipx install snowflake-cli-labs==$CLI_VERSION 
 fi
 
+extension=""
+if [[ $RUNNER_OS = "Windows" ]]; then 
+    extension=".exe"
+fi
 
 mkdir "$PIPX_BIN_DIR/snow_pipx_path"
-cp "$PIPX_BIN_DIR/snow" "$PIPX_BIN_DIR/snow_pipx_path"
+cp "$PIPX_BIN_DIR/snow$extension" "$PIPX_BIN_DIR/snow_pipx_path"
 echo "$PIPX_BIN_DIR/snow_pipx_path" >> $GITHUB_PATH
-
-echo $(ls "$PIPX_BIN_DIR")
-echo $(ls "$PIPX_BIN_DIR/snow_pipx_path")
-
-
 
 
