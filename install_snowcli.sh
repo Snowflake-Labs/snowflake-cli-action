816ef6173e86a70a8f2af8a01f67a66a1514e38c
Jorge Vasquez Rojas <jorge.vasquezrojas@snowflake.com>
2024-04-03 09:23:17 -0600

    Show pipx path in windows

--- install_snowcli.sh
+++ install_snowcli.sh
@@ -13,5 +13,9 @@ mkdir "$PIPX_BIN_DIR/snow_pipx_path"
 cp "$PIPX_BIN_DIR/snow" "$PIPX_BIN_DIR/snow_pipx_path"
 echo "$PIPX_BIN_DIR/snow_pipx_path" >> $GITHUB_PATH
 
+echo $(ls $PIPX_BIN_DIR)
+echo $(ls $PIPX_BIN_DIR/snow_pipx_path)
+
+
 
 
