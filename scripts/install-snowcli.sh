30d95971d1e821d677c556047858859a9391eda3
Jorge Vasquez Rojas <jorge.vasquezrojas@snowflake.com>
2024-06-25 16:15:56 -0600

    Update shell

--- scripts/install-snowcli.sh
+++ scripts/install-snowcli.sh
@@ -2,9 +2,17 @@
 
 set -euo pipefail
 
+# These commands ensure that each time `snow` command is executed the system will use 
+# the executable in the pipx installation folder and not in any other installation folder.
 if [ -z "${PIPX_BIN_DIR}" ]; then
-    export PIPX_BIN_DIR="~/.local/bin"
+    export PIPX_BIN_DIR="~/.local/bin/snow_pipx_path"
+else 
+    export PIPX_BIN_DIR="$PIPX_BIN_DIR/snow_pipx_path"
 fi
+
+mkdir -p "$PIPX_BIN_DIR"
+
+
 
 if [ "$CLI_VERSION" == "latest" ]; then
     pipx install snowflake-cli-labs 
@@ -12,13 +20,5 @@ else
     pipx install snowflake-cli-labs==$CLI_VERSION 
 fi
 
-extension=""
-if [[ $RUNNER_OS = "Windows" ]]; then 
-    extension=".exe"
-fi
 
-# These commands ensure that each time `snow` command is executed the system will use 
-# the executable in the pipx installation folder and not in any other installation folder.
-mkdir "$PIPX_BIN_DIR/snow_pipx_path"
-cp "$PIPX_BIN_DIR/snow$extension" "$PIPX_BIN_DIR/snow_pipx_path"
-echo "$PIPX_BIN_DIR/snow_pipx_path" >> $GITHUB_PATH
+echo "$PIPX_BIN_DIR" >> $GITHUB_PATH
