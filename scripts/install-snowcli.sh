988d8ac62aba7e8914b516f7096f525cf6bc2135
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2024-08-15 13:29:47 -0700

    Remove PIPX_PATH variable
    This is not a standard variable used by pipx

--- scripts/install-snowcli.sh
+++ scripts/install-snowcli.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
 
 set -euo pipefail
-PIPX_PATH="snow_pipx_path"
 
 # These commands ensure that each time `snow` command is executed the system will use 
 # the executable in the pipx installation folder and not in any other installation folder.
 
-PIPX_BIN_DIR=${PIPX_BIN_DIR:-"~/.local/bin"}/$PIPX_PATH
+PIPX_BIN_DIR=${PIPX_BIN_DIR:-"~/.local/bin"}
 
 mkdir -p "${PIPX_BIN_DIR}"
 
