d3d76ac49d09d46c6c0eb3cfacdb6db374fdf551
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2024-08-15 13:18:01 -0700

    Fix unbound variable PIPX_BIN_DIR
    Closes #21

--- scripts/install-snowcli.sh
+++ scripts/install-snowcli.sh
@@ -1,17 +1,14 @@
-﻿#!/bin/sh
+﻿#!/usr/bin/bash
 
 set -euo pipefail
 PIPX_PATH="snow_pipx_path"
 
 # These commands ensure that each time `snow` command is executed the system will use 
 # the executable in the pipx installation folder and not in any other installation folder.
-if [ -z "${PIPX_BIN_DIR}" ]; then
-    export PIPX_BIN_DIR="~/.local/bin/$PIPX_PATH"
-else 
-    export PIPX_BIN_DIR="$PIPX_BIN_DIR/$PIPX_PATH"
-fi
 
-mkdir -p "$PIPX_BIN_DIR"
+PIPX_BIN_DIR=${PIPX_BIN_DIR:-"~/.local/bin"}/$PIPX_PATH
+
+mkdir -p "${PIPX_BIN_DIR}"
 
 
 if [ "$CLI_VERSION" == "latest" ]; then
