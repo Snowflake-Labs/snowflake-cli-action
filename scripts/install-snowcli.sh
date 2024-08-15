24ae99495c215fc750c8e7ac2db9e9589f2d76ee
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2024-08-15 14:56:01 -0700

    Revert "Remove PIPX_PATH variable"
    This reverts commit 988d8ac62aba7e8914b516f7096f525cf6bc2135.

--- scripts/install-snowcli.sh
+++ scripts/install-snowcli.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
 set -euo pipefail
+PIPX_PATH="snow_pipx_path"
 
 # These commands ensure that each time `snow` command is executed the system will use 
 # the executable in the pipx installation folder and not in any other installation folder.
 
-PIPX_BIN_DIR=${PIPX_BIN_DIR:-"~/.local/bin"}
+PIPX_BIN_DIR=${PIPX_BIN_DIR:-"~/.local/bin"}/$PIPX_PATH
 
 mkdir -p "${PIPX_BIN_DIR}"
 
