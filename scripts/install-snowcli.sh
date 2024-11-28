4a291c7040f1e4d2feb5bfa316e1ad5099cffa63
Jorge Vasquez Rojas <jorge.vasquezrojas@snowflake.com>
2024-11-28 09:22:14 -0600

    Use installed pythonpah

--- scripts/install-snowcli.sh
+++ scripts/install-snowcli.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 PIPX_PATH="snow_pipx_path"
+PYTHON_PATH=$(python -c "import sys; print(sys.executable)")
 
 # These commands ensure that each time `snow` command is executed the system will use 
 # the executable in the pipx installation folder and not in any other installation folder.
@@ -12,9 +13,9 @@ mkdir -p "${PIPX_BIN_DIR}"
 
 
 if [ "$CLI_VERSION" == "latest" ]; then
-    pipx install snowflake-cli --python $(which python)
+    pipx install snowflake-cli --python "$PYTHON_PATH"
 else 
-    pipx install snowflake-cli=="$CLI_VERSION" --python $(which python)
+    pipx install snowflake-cli=="$CLI_VERSION" --python "$PYTHON_PATH"
 fi
 
 
